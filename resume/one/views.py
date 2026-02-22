fba4344e3ada540881e882173538cce46684b74c
aswanth-mc <mcaswanth5@gmail.com>
2026-02-22 13:50:04 +0530

    feat : add education to dict

--- resume/one/views.py
+++ resume/one/views.py
@@ -5,5 +5,11 @@ resume = {
     "name": "aswanth",
     "age": 21,
     "gender":"male",
-    "skills": ["python","django","html","css"]
+    "skills": ["python","django","html","css"],
+    "education": {
+            "degree": "Bachelor of Computer Applications",
+            "college": "Calicut University",
+            "year": 2025
+        }
+    
 }
