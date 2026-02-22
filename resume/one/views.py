1d9d81423a4b55d6d8f0ac3a762386cc2d545694
aswanth-mc <mcaswanth5@gmail.com>
2026-02-22 10:44:19 +0530

    feat : add a dictory and add some key value pair

--- resume/one/views.py
+++ resume/one/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+resume = {
+    "name": "aswanth",
+    "age": 21,
+    "gender":"male",
+    "skills": ["python","django","html","css"]
+}
